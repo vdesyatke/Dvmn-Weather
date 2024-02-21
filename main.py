b93eac2fa7152f2d4a2a02524cd0b86a8f227aae
vdesyatke <72273263+vdesyatke@users.noreply.github.com>
2024-02-21 17:25:29 +0400

    removed unused code, added argparse, moved url parameters from url to payload, added raised_for_status checkup

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import requests
+import argparse
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Description: this program prints out Weather in London, Cherepovets, Sheremetyevo'
+    )
+    parser.parse_args()
+
     places = ['Лондон', 'Шереметьево', 'Череповец']
+    payload = {'nTqmM': '', 'lang': 'ru'}
     for place in places:
-        url = f'https://www.wttr.in/{place}?nTqmM&lang=ru'
-        response = requests.get(url)
+        url = f'https://www.wttr.in/{place}'
+        response = requests.get(url, params=payload)
+        response.raise_for_status()
         print(response.text)
+
